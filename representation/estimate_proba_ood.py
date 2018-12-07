5f96f391bd9508cae7ecf7b389d681481ed57083
nbrosse <nbrosse@gmail.com>
2018-12-07 09:38:26 +0100

    small change

--- representation/estimate_proba_ood.py
+++ representation/estimate_proba_ood.py
@@ -30,28 +30,44 @@ def estimates_mnist():
   
   
 def estimates_cifar10():
-  output_dir = 'outputs/one_point_estimates/'
+  output_dir = 'outputs_ood/one_point_estimates/cifar10_sec_5'
   (x_train, y_train), (x_test, y_test) = cifar.input_cifar10()
-  model = cifar.build_model(x_train, 10)
-  model_path = 'saved_models/keras_cifar10_trained_model.h5'
+  
+  index_path = 'saved_models_ood/cifar10_sec_5/index.npy'
+  index = np.load(index_path)
+  n_class = np.sum(index)
+  indices_test_in = np.dot(y_test, index).astype(bool)
+  
+  model = cifar.build_model(x_train, n_class)
+  model_path = 'saved_models_ood/cifar10_sec_5/keras_cifar10_trained_model.h5'
   model.load_weights(model_path)
   # Predict
-  proba = model.predict(x_test)
+  proba_in = model.predict(x_test[indices_test_in, :])
+  proba_out = model.predict(x_test[~indices_test_in, :])
   # Saving the probabilities
-  np.save(os.path.join(output_dir, 'p_cifar10.npy'), proba)
+  np.save(os.path.join(output_dir, 'p_cifar10_in.npy'), proba_in)
+  np.save(os.path.join(output_dir, 'p_cifar10_out.npy'), proba_out)
   
   
 def estimates_cifar100():
-  output_dir = 'outputs/one_point_estimates/'
+  output_dir = 'outputs_ood/one_point_estimates/cifar100_sec_50'
   (x_train, y_train), (x_test, y_test) = cifar.input_cifar100()
-  model = cifar100.build_model(x_train, 100)
-  model_path = 'saved_models/andrewkruger_cifar100.h5'
+  
+  index_path = 'saved_models_ood/cifar100_sec_50/index.npy'
+  index = np.load(index_path)
+  n_class = np.sum(index)
+  indices_test_in = np.dot(y_test, index).astype(bool)
+  
+  model = cifar100.build_model(x_train, n_class)
+  model_path = 'saved_models_ood/cifar100_sec_50/andrewkruger_cifar100.h5'
   model.load_weights(model_path)
   # Predict
-  proba = model.predict(x_test)
+  proba_in = model.predict(x_test[indices_test_in, :])
+  proba_out = model.predict(x_test[~indices_test_in, :])
   # Saving the probabilities
-  np.save(os.path.join(output_dir, 'p_cifar100.npy'), proba)
+  np.save(os.path.join(output_dir, 'p_cifar100_in.npy'), proba_in)
+  np.save(os.path.join(output_dir, 'p_cifar100_out.npy'), proba_out)
   
-estimates_mnist()
+#estimates_mnist()
 #estimates_cifar10()
-#estimates_cifar100()
+estimates_cifar100()
